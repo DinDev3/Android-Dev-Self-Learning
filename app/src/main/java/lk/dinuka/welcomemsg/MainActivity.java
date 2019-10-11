d4e259798589e84d31f8f71ae533a77ba435f594
Dinuka <drpiyadigama@gmail.com>
2019-10-11 23:50:10 +0530

    Created an application that gets input and displays a welcome note, on button click

--- app/src/main/java/lk/dinuka/welcomemsg/MainActivity.java
+++ app/src/main/java/lk/dinuka/welcomemsg/MainActivity.java
@@ -3,6 +3,10 @@ package lk.dinuka.welcomemsg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button)this.findViewById(R.id.click_btn);
+        b.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)  {
+                TextView resp = (TextView) findViewById(R.id.response);
+                EditText name = (EditText) findViewById(R.id.user_name);
+                String str = "Welcome " + name.getText().toString() + " !";
+                resp.setText(str);
+            }
+        });
     }
 }
